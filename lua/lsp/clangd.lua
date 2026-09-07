3b8754625230192220e67c09a2fc6373659f510d
Nicolas Ucieda <nicolas.ucieda@gmail.com>
2026-09-07 10:14:11 +0200

    Remove c++17 from fallback flags for clangd

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -10,7 +10,6 @@ vim.lsp.config('clangd', {
     },
     filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
     init_options = {
-        fallbackFlags = { '-std=c++17' },
         offsetEncoding = 'utf-8',
     },
 })
